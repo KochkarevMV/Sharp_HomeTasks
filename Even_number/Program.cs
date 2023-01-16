87a4c297e7cddec6d95ce49098fb0eab6673af8b
KochkarevMV <120572414+KochkarevMV@users.noreply.github.com>
2023-01-16 15:42:08 +0300

    Even number ready

---
+++ Even_number/Program.cs
@@ -0,0 +1,11 @@
+﻿// Программа по выявлению чётного или нечётного числа
+
+Console.Clear();
+Console.WriteLine("Давайте определим чётное ли, или нечётное ваше число");
+Console.Write("Введите число:");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a%2 == 0)
+Console.Write("Ваше число является чётным");
+else if (a%2 != 0)
+Console.Write("Ваше число является нечётным");
