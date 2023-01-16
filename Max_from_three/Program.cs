cb4f56b7ebd17dea2e61f3e3f53386adb996497b
KochkarevMV <120572414+KochkarevMV@users.noreply.github.com>
2023-01-16 15:28:18 +0300

    Max from three ready

--- Max_from_three/Program.cs
+++ Max_from_three/Program.cs
@@ -1,4 +1,5 @@
 ﻿// Программа по нахождению максимального из трёх чисел
+
 Console.Clear();
 Console.WriteLine("Давайте определим максимум из трёх чисел");
 Console.Write("Введите первое число:");
@@ -6,5 +7,13 @@ int a = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число:");
 int b = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите третье число:");
-int с = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
 
+if (a > b && a > c)
+Console.WriteLine("Первое число является наибольшим значением");
+else if (b > c && b > a)
+Console.WriteLine("Второе число является наибольшим значением");
+else if (c > b && c > a)
+Console.WriteLine("Третье число является наибольшим значением");
+else
+Console.WriteLine("Максимальные числа равны - максимальных чисел больше, чем одно: введите новые (отличные друг от друга) числа");
