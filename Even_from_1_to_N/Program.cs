14c6316c2f66dd23d6b2d543a0c4424fcf64f8cd
KochkarevMV <120572414+KochkarevMV@users.noreply.github.com>
2023-01-16 16:29:08 +0300

    Even from 1 to N fixed

--- Even_from_1_to_N/Program.cs
+++ Even_from_1_to_N/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Программа по нахождению массива чётных чисел от 1 до указанного числа
+
+Console.Clear();
+Console.WriteLine("Давайте определим массив чётных чисел от 1 до указанного вами числа");
+Console.Write("Введите число:");
+int N = Convert.ToInt32(Console.ReadLine());
+int M = 0;
+
+while (M <= N && N%2 == 0)
+{
+    Console.Write($"{N}, ");
+    M = M++;
+}
