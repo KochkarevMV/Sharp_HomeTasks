5ed4f10ca86c5b0d6ab95aa4879dd334cc322af5
KochkarevMV <120572414+KochkarevMV@users.noreply.github.com>
2023-01-16 15:12:34 +0300

    Max from three added

---
+++ Max_from_three/Program.cs
@@ -0,0 +1,10 @@
+﻿// Программа по нахождению максимального из трёх чисел
+Console.Clear();
+Console.WriteLine("Давайте определим максимум из трёх чисел");
+Console.Write("Введите первое число:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число:");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число:");
+int с = Convert.ToInt32(Console.ReadLine());
+
