fc5e77a295abfb2c89067a9180efa4ec0548d947
KochkarevMV <120572414+KochkarevMV@users.noreply.github.com>
2023-01-16 15:09:47 +0300

    Max from two ready

--- Max_from_two/Program.cs
+++ Max_from_two/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Программа по нахождению максимального из двух чисел
+
+Console.Clear();
+Console.WriteLine("Давайте определим максимум из двух чисел");
+Console.Write("Введите первое число:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число:");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+Console.Write("Первое число больше второго");
+else if (a < b)
+Console.Write("Второе число больше первого");
+else
+Console.Write("Числа равны");
