1c0a9c6b01e8bd067df6c79b2155019d2375a0a1
KochkarevMV <120572414+KochkarevMV@users.noreply.github.com>
2023-01-17 14:47:05 +0300

    Вторая_цифра_3х_значного_числа Ready

---
+++ Вторая_цифра_3х_значного_числа/Program.cs
@@ -0,0 +1,9 @@
+﻿// Программма по нахождению второй цифры заданного пользователем трёхзначного числа
+
+Console.Clear();
+Console.WriteLine("Давайте определим вторую цифру ваше числа");
+Console.WriteLine("Введите трёхзначное число");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = (a%100)/10;
+Console.Write("Вторая цифра вашего числа: ");
+Console.Write($"{b}");
