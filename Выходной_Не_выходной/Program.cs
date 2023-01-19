bcabc793e9d98a5cbf3ca1409de4839bd9a954f9
KochkarevMV <120572414+KochkarevMV@users.noreply.github.com>
2023-01-19 12:56:54 +0300

    Выходной не выходной added

---
+++ Выходной_Не_выходной/Program.cs
@@ -0,0 +1,13 @@
+﻿// Программма по определению выходного/не входного дня по заданному пользователем числу
+
+Console.Clear();
+Console.WriteLine("Давайте определим выходной ли день вы ввели");
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a > 0 && a < 6)
+Console.Write("Это будний день");
+else if (a > 5 && a < 8)
+Console.Write("Это выходной день");
+else
+Console.Write("Введите число от 1 до 7");
