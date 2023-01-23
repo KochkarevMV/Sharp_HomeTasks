663ed143fdfba017d1b3cce2ac464bb42e4346a9
KochkarevMV <120572414+KochkarevMV@users.noreply.github.com>
2023-01-23 14:50:44 +0300

    Cubes Table  added

---
+++ Cubes_Table/Program.cs
@@ -0,0 +1,11 @@
+﻿//  Программа принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.Clear();
+Console.WriteLine("Давайте определим таблицу кубов вашего числа");
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+while (a > count)
+{
+    
+}
