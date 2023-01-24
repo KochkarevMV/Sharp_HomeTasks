5fa026cddeff5d12e4ec758f6afb419b5260914c
Max Oldey <120572414+KochkarevMV@users.noreply.github.com>
2023-01-24 10:06:42 +0300

    Решение_преподавателя_1

--- Palindrom/Program.cs
+++ Palindrom/Program.cs
@@ -8,3 +8,16 @@ if (a >= 10000 && a < 100000 && a/10000 == a%10 && (a%100)/10 == (a%10000)/1000)
 Console.WriteLine("Число является полиндромом");
 else
 Console.WriteLine("Число не является полиндромом");
+
+// Решение преподавателя
+// Console.Clear();
+// Console.Write("Введите число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int n1 = n / 10000;
+// int n2 = (n / 1000) % 10;
+// int n4 = (n % 100) / 10;
+// int n5 = n % 10;
+// if (n1 == n5 && n2 == n4)
+//     Console.WriteLine("yes");
+// else
+//     Console.WriteLine("no");
