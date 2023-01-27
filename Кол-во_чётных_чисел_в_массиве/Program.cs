9cd18f0ed0511a87aa15ec844944f799c318f7a7
KochkarevMV <ufomusic@mail.ru>
2023-01-27 18:13:22 +0300

    Кол-во_чётных_чисел_в_массиве Ready

---
+++ Кол-во_чётных_чисел_в_массиве/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+Console.Clear();
+void Mass (int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(100,1000);
+}
+
+int MassChet (int [] array)
+{
+    int count = 0;
+    foreach (int element in array)
+    {
+        if(element%2 == 0)
+        count++;
+    }
+    return count;
+}
+
+Console.WriteLine("Введите количество чисел в массиве: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int [] OurMass = new int [a];
+Mass(OurMass);
+Console.WriteLine($"Ваш массив: [{string.Join(",",OurMass)}] ");
+Console.WriteLine($"Количество чётных чисел:");
+Console.WriteLine(MassChet(OurMass));
