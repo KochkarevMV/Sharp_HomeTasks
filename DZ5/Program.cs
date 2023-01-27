ee5a88e4a05e11cf90cb347506de4890caee5982
KochkarevMV <ufomusic@mail.ru>
2023-01-27 14:08:25 +0300

    DZ5

---
+++ DZ5/Program.cs
@@ -0,0 +1,43 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// Math.Pow(ЗАПРЕЩЕНО)
+
+// Console.Clear();
+// Console.WriteLine("Введите число 1: ");
+// int A = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число 2: ");
+// int B = Convert.ToInt32(Console.ReadLine());
+// int C = 1;
+// for (int i = 1; i <= B; i++)
+// C = A*C;
+// Console.WriteLine($"{C}");
+
+
+
+
+// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// Console.Clear();
+// Console.WriteLine("Введите число: ");
+// int A = Convert.ToInt32(Console.ReadLine());
+// int B = 0;
+// while (A > 0)
+// {
+//     B = B + A%10;
+//     A = A/10;
+// }
+// Console.WriteLine(B);
+
+
+
+//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран(ввод пользователя с клавиатуры).
+//1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+
+// Console.Clear();
+// int [] array = new int [8];
+// for (int i = 0; i < array.Length; i++)
+// {
+//     Console.WriteLine("Введите число: ");
+//     array[i] = Convert.ToInt32(Console.ReadLine());
+// }
+// Console.WriteLine($"Ваш массив: [{string.Join("|", array)}]");
+
