573826e7e3189d44be65c1c5a68652dded437137
KochkarevMV <120572414+KochkarevMV@users.noreply.github.com>
2023-01-31 18:46:01 +0300

    Пересечение 1

--- Пересечение_двух_прямых/Program.cs
+++ Пересечение_двух_прямых/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2. 
+// Это уравнение прямой пропорциональности
+
+// Значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+
+Console.Clear();
+Console.WriteLine("Введите значение b1: ");
+int b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение k1: ");
+int k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение b2: ");
+int b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение k2: ");
+int k2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Точка b1 = {b1}");
+Console.WriteLine($"Точка k1 = {k1}");
+Console.WriteLine($"Точка b2 = {b1}");
+Console.WriteLine($"Точка k2 = {b2}");
+
