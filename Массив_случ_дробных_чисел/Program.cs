d04389bd017d9d028fecd5a4fad6e9c57cb10532
KochkarevMV <ufomusic@mail.ru>
2023-02-05 20:00:37 +0300

    Массив_случ_дроб_2

--- Массив_случ_дробных_чисел/Program.cs
+++ Массив_случ_дробных_чисел/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+
+
+void FillArray (double [,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = Math.Round(new Random().NextDouble() * (5 - 1) + 1, 1);
+        }
+    }
+}
+
+void PrintArray (double [,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    Console.Write($"[{array[i,j]}]");
+Console.WriteLine();
+}
+}
+
+Console.Clear();
+Console.WriteLine("Введите размеры массива: ");
+int[] size = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+double [,] matrix = new double[size[0], size[1]];
+FillArray(matrix);
+PrintArray(matrix);
