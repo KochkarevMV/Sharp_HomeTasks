5f870e08e0e9fc649343cbfb848dba9a96126e2a
KochkarevMV <ufomusic@mail.ru>
2023-02-07 10:15:13 +0300

    Нахождение элемента Ready

--- Нахождение_эл-та_двумерн_массива/Program.cs
+++ Нахождение_эл-та_двумерн_массива/Program.cs
@@ -8,37 +8,37 @@
 
 // 1 7 -> такой позиции в массиве нет
 
-void FillArray (int [,] array)
+void InputMatrix(double[,] matrix)
 {
-    for(int i = 0; i < array.GetLength(0); i++)
+    for (int i = 0; i < matrix.GetLength(0); i++)
     {
-        for(int j = 0; j < array.GetLength(1); j++)
+        for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            array[i,j] = new Random().Next(1,10);
-            Console.Write(array[i,j]);
+            matrix[i, j] = Math.Round(new Random().NextDouble() * (20 + 20) - 20, 2);
+            Console.Write($"{matrix[i, j]} \t");
         }
         Console.WriteLine();
     }
 }
 
-int Finder (int [,] array)
-{
-    Console.WriteLine("Введите позицию элемента в массиве: ");
-    int [] n = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
-    int x = 0;
-    string y = "Такого элемента нет в массиве";
-    for(int i = 0; i < array.GetLength(0); i++)
-    {
-        for(int j = 0; j < array.GetLength(0); j++)
-        {
-            if (i <= n[0] && j <= n[1])
-            x = array[n[0], n[1]];    
-        }
-    }
-return x;
-}
-    
 
-int [,] MyArray = new int [3,3];
-FillArray(MyArray);
-Console.WriteLine(Finder(MyArray));
+double SearchElementToMatrix(double[,] matrix, int row, int column)
+{
+    if (matrix.GetLength(0) > row && matrix.GetLength(1) > column && row >= 0 && column >= 0)
+        return matrix[row, column];
+    return 0;
+}
+
+
+Console.Clear();
+Console.Write("Введите размеры массива: ");
+int[] size = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+double[,] matrix = new double[size[0], size[1]];
+InputMatrix(matrix);
+int[] coord = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+Console.Write("Введите позицию элемента: ");
+double result = SearchElementToMatrix(matrix, coord[0] - 1, coord[1] - 1);
+if (result != 0)
+    Console.WriteLine(result);
+else
+    Console.WriteLine("Такого числа нет");
