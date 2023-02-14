b71f372e0d9b59a23b6b3032441d58a2d9368c0f
KochkarevMV <120572414+KochkarevMV@users.noreply.github.com>
2023-02-14 12:18:26 +0300

    Упорядочить массив Ready 2

--- Упорядочить_по_убыванию_массив/Program.cs
+++ Упорядочить_по_убыванию_массив/Program.cs
@@ -42,7 +42,7 @@ void ReleaseMatrix(int[,] matrix)
 
 
 Console.Clear();
-Console.Write("Введите размер маcсива: ");
+Console.Write("Введите размер массива: ");
 int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
 int[,] matrix = new int[size[0], size[1]];
 InputMatrix(matrix);
