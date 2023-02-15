ce162bf1a7d25a44c2d5b7ef8da6c4b2b9c50f78
KochkarevMV <ufomusic@mail.ru>
2023-02-15 11:43:51 +0300

    Сумма натур эл Ready

---
+++ Сумма_натур_эл_в_промежутке/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int f(int m, int n)
+{
+  if (m > n)
+    return 0;
+  return n + f(m, n - 1);
+}
+
+Console.Clear();
+Console.Write("Введите 1-ое число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-ое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(f(m, n));
